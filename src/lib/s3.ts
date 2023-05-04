9983a64c7e47c1fde9721c17bc4a7d68b66e5b7d
박지호 <yoho4613@gmail.com>
2023-05-04 16:05:15 +1200

    update env

--- src/lib/s3.ts
+++ src/lib/s3.ts
@@ -3,6 +3,6 @@ import S3 from "aws-sdk/clients/s3";
 export const s3 = new S3({
   accessKeyId: process.env.AWS_PUBLIC_ACCESS_KEY_ID,
   secretAccessKey: process.env.AWS_SECRECT_ACCESS_PUBLIC_KEY,
-  region: process.env.AWS_REGION,
+  region: process.env.AWS_REGION_PUBLIC,
   signatureVersion: "v4",
 });
