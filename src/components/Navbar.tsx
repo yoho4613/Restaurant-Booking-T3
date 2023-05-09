0233073906bc5a7b8d1f20689ad0e6c6b3c4d14e
박지호 <yoho4613@gmail.com>
2023-05-09 12:53:17 +1200

    admin login nav added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -99,6 +99,14 @@ const Navbar: FC = ({}) => {
                   Contact
                 </Link>
               </li>
+              <li>
+                <Link
+                  href="/login"
+                  className="block rounded py-2 pl-3 pr-4 hover:bg-gray-100 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:p-0 text-red-600 font-bold font-mono md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-blue-500"
+                >
+                  Admin Login
+                </Link>
+              </li>
             </ul>
           </div>
         </div>
