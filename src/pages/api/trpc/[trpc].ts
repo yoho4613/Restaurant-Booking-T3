6c47885449ef2579baf115c329fd238553534fb0
박지호 <yoho4613@gmail.com>
2023-07-03 23:17:16 +1200

    remove json protocol

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -8,14 +8,12 @@ import { appRouter } from "~/server/api/root";
 export default createNextApiHandler({
   router: appRouter,
   createContext: createTRPCContext,
-  onError: ({ path, error }) => {
-    console.error(`❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`);
-  },
-  // env.NODE_ENV === "development"
-  //   ? ({ path, error }) => {
-  //       console.error(
-  //         `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-  //       );
-  //     }
-  //   : undefined,
+  onError:
+    env.NODE_ENV === "development"
+      ? ({ path, error }) => {
+          console.error(
+            `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
+          );
+        }
+      : undefined,
 });
