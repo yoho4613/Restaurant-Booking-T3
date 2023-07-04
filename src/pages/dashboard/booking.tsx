b7b1f1b04bdc960ecbe4f02c35e4107ecb168549
박지호 <yoho4613@gmail.com>
2023-07-05 01:48:14 +1200

    booking search bar padding

--- src/pages/dashboard/booking.tsx
+++ src/pages/dashboard/booking.tsx
@@ -332,7 +332,7 @@ const Booking: FC<BookingProps> = ({ days, closedDays }) => {
             <input
               type="text"
               id="simple-search"
-              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-1.5 md:p-2.5 pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
+              className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-1.5 md:p-2.5 pl-10 md:pl-10 text-sm text-gray-900 focus:border-blue-500 focus:ring-blue-500  dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400 dark:focus:border-blue-500 dark:focus:ring-blue-500"
               placeholder="Search"
               onChange={(e) => {
                 setFilter(e.target.value);
