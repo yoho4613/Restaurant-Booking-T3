c4aa772c1a79b0506ce2720b650d07a6f2494883
박지호 <yoho4613@gmail.com>
2023-10-13 18:07:56 +1300

    success page

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -50,14 +50,23 @@ const Success: FC = ({}) => {
       <div>
         <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8 lg:py-32 xl:gap-x-24">
           <div className="lg:col-start-2">
-            <h1 className="text-sm font-medium text-indigo-600">
-              Payment successful
-            </h1>
-            <p className="mt-2 text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
-              Thanks for ordering
-            </p>
+            {products ? (
+              <>
+                <h1 className="text-sm font-medium text-indigo-600">
+                  Payment successful
+                </h1>
+
+                <p className="mt-2 text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
+                  Thanks for ordering
+                </p>
+              </>
+            ) : (
+              <p className="mt-2 text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl">
+                Thanks for Booking
+              </p>
+            )}
             <p className="mt-2 text-base text-gray-500">
-              We appreciate your order, we’re currently processing it. So hang
+              We appreciate your booking, we’re currently processing it. So hang
               tight and we’ll send you confirmation very soon!
             </p>
 
